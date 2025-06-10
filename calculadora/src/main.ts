7cd49a10958c733d5858ad61965260aedc8962ab
Kevin Quishpe <kevin.quishpe04@epn.edu.ec>
2025-06-09 21:37:54 -0500

    Segundo commit de workflows

--- calculadora/src/main.ts
+++ calculadora/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap()
+  .then((port) => {
+    Logger.log(
+      `✅ Servidor corriendo en http://localhost:${port}`,
+      'Bootstrap',
+    );
+  })
+  .catch((error) => {
+    Logger.error('❌ Error al iniciar el servidor:', error, 'Bootstrap');
+    process.exit(1);
+  });
--- calculadora/test/app.e2e-spec.ts
+++ calculadora/test/app.e2e-spec.ts
@@ -7,6 +7,15 @@ import { AppModule } from './../src/app.module';
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
 
+  describe('root', () => {
+    it('should return "Hello World!!"', () => {
+      return request(app.getHttpServer())
+        .get('/')
+        .expect(200)
+        .expect('Hello World!!');
+    });
+  });
+
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
