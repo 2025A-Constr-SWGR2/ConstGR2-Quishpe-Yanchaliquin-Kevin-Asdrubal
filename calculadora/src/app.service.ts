02c91ceffc16e6b5e7b8d38e0c90589d10e03a6b
Kevin Quishpe <kevin.quishpe04@epn.edu.ec>
2025-06-13 20:34:58 -0500

    Correción errores

--- calculadora/src/app.service.ts
+++ calculadora/src/app.service.ts
@@ -3,11 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    console.log('log!');
-    console.debug('debug!');
-    console.info('info!');
-    console.warn('warn!');
-    console.error('error!');
-    return 'Hello World!!';
+    console.warn('warn!');    // ✅ Permitido
+    console.error('error!');  // ✅ Permitido
+    return 'Hello World!';
   }
 }
